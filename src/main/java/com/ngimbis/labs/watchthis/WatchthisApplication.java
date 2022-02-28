ee6386c0f206e428b57f36d8ac8c11f9ffa7d42e
NG P <ngimbis@dpc-sa.com>
2022-02-28 11:48:23 +0100

    Project initialization

---
+++ src/main/java/com/ngimbis/labs/watchthis/WatchthisApplication.java
@@ -0,0 +1,13 @@
+package com.ngimbis.labs.watchthis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchthisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchthisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ngimbis/labs/watchthis/WatchthisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngimbis.labs.watchthis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchthisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
